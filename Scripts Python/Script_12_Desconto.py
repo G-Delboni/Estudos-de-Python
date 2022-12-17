8f0c92e1e62ad2623d39c485ebc3ef37314b4aa7
G-Delboni <ygabrieldelboni.contato@gmail.com>
2022-12-16 23:36:37 -0300

    Revert "Update Scripts_10-15"
    This reverts commit 33428f2aeefde65c6a0eea392c9fceaa104a5ec7.

--- Scripts Python/Script_12_Desconto.py
+++
@@ -1,6 +0,0 @@
-valortotal = float(input('Digite o valor do produto R$'))
-
-desconto = valortotal * 0.05
-valornovo = valortotal - desconto
-
-print(f'O produto de R${valortotal:.2f}, com 5% de desconto, custará R${valornovo:.2f}')
--- Scripts Python/Script_13_Salario.py
+++
@@ -1,7 +0,0 @@
-salario = float(input('Digite o salário do funcionário '))
-print(f'O salário do funcionário era de R${salario:.2f}')
-
-aumento = salario * 0.15 #Ou salario * 15 / 100
-salario = salario + aumento
-
-print(f'O novo salário do funcionário será de R${salario:.2f}')
