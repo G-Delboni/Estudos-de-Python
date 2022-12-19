8f5f55355fb3a2894061f88d03a15ba2b8cf232f
G-Delboni <ygabrieldelboni.contato@gmail.com>
2022-12-19 13:43:10 -0300

    Update exercises 023

--- Scripts Python/Script_023_SeparandoDigitos.py
+++ Scripts Python/Script_023_SeparandoDigitos.py
@@ -1,3 +1,5 @@
-num = input('Digite um número de 0 a 9999: ')
+num = int(input('Digite um número de 0 a 9999: '))
+n2 = str(int(10000 + num)) # A variável num é incorporada à variável n2, portanto se for menor que o número total de dígitos o código não quebra, já que o print vai exibir o valor "0" para as casas que estão faltando.
 
-print(f'Unidade: {num[3:]}\nDezena: {num[2:3]}\nCentena: {num[1:2]}\nMilhar: {num[0:1]}')
+
+print(f'Unidade: {n2[4]}\nDezena: {n2[3]}\nCentena: {n2[2]}\nMilhar: {n2[1]}')
