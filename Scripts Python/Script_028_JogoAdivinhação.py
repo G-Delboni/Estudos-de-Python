aaf96a533ed21dbd2980eb3a7229a4e468ad53fa
G-Delboni <ygabrieldelboni.contato@gmail.com>
2022-12-29 19:28:58 -0300

    Att visual

--- Scripts Python/Script_028_JogoAdivinhação.py
+++ Scripts Python/Script_028_JogoAdivinhação.py
@@ -1,7 +1,9 @@
 from random import randint
 
 numero_computador = randint(0, 5)
-numero_digitado = int(input('Qual o número que estou pensando? '))
+print('-=-' * 20)
+numero_digitado = int(input('Qual o número entre 0 e 5 que estou pensando? '))
+print('-=-' * 20)
 
 print(f'O número que você digitou é: {numero_digitado} ')
 print(f'O número que estou pensando é: {numero_computador} \nPortanto, você: ')
